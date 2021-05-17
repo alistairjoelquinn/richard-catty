7b8d9b32f2c4add443b2699be325c0143932dcb6
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-05-17 17:44:40 +0200

    adds: all content for services page

--- client/pages/services.js
+++ client/pages/services.js
@@ -1,15 +1,30 @@
 import Head from 'next/head';
 import styled from 'styled-components';
 
+import services from '../content/services.json';
+
 const ServicesPageStyles = styled.div`
     height: 100vh;
     width: 100vw;
     display: flex;
-    align-items: center;
-    justify-content: center;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    font-family: Cutive;
     background-image: url('https://res.cloudinary.com/dtirfwiy8/image/upload/q_10/v1619200419/IMG_8033-2_g3bw8f.jpg');
     background-position: top right;
     background-size: cover;
+    ul {
+        padding: 15vh 5vw;
+        li {
+            font-size: 2.5rem;
+            padding: 2rem;
+            &:nth-child(1) {
+                list-style-type: none;
+                transform: translateX(-4rem);
+            }
+        }
+    }
 `;
 
 const ServicesPage = () => {
@@ -23,7 +38,14 @@ const ServicesPage = () => {
                     content="https://res.cloudinary.com/dtirfwiy8/image/upload/q_10/v1619200419/IMG_8033-2_g3bw8f.jpg"
                 />
             </Head>
-            <ServicesPageStyles></ServicesPageStyles>
+            <ServicesPageStyles>
+                <ul>
+                    <li>Services I provide. Please email for a quote.</li>
+                    {services.map(service => (
+                        <li key={service}>{service}</li>
+                    ))}
+                </ul>
+            </ServicesPageStyles>
         </>
     );
 };
