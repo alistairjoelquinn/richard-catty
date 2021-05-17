c7d0d75968b463f8dcd721d02a05ed0f471515e7
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-05-17 17:56:28 +0200

    updates: og tags to include twitter tags

--- client/components/Page.js
+++ client/components/Page.js
@@ -20,6 +20,15 @@ const Page = ({ children }) => {
                 {typeof window === 'object' && <meta property="og:url" content={location?.href} />}
                 <meta property="og:site_name" content={meta.title} key="ogsitename" />
                 <meta property="og:description" content={meta.description} key="ogdesc" />
+                <meta name="twitter:card" content="summary_large_image" />
+                {typeof window === 'object' && <meta property="twitter:domain" content="richard-catty.vercel.app" />}
+                <meta property="twitter:url" content={location?.href} />
+                <meta name="twitter:title" content={meta.title} />
+                <meta name="twitter:description" content={meta.description} />
+                <meta
+                    name="twitter:image"
+                    content="https://res.cloudinary.com/dtirfwiy8/image/upload/q_10/v1619200431/IMG_8101-3_rpodzj.jpg"
+                />
             </Head>
             <Normalize />
             <GlobalStyles />
