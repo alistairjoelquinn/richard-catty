6dac85d5656b466ea046ea997766b64c63b31910
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-05-27 14:35:55 +0200

    portfolio items glow and blur correctly

--- client/components/PortfolioItem.js
+++ client/components/PortfolioItem.js
@@ -8,28 +8,52 @@ const ReadMoreTextStyles = styled.div`
     position: absolute;
     height: 100%;
     width: 100%;
-    backdrop-filter: blur(5px);
+    backdrop-filter: none;
     z-index: 10;
     display: flex;
     justify-content: center;
     align-items: center;
     font-size: 2.6rem;
     font-weight: bold;
-    color: black;
+    color: #1f1f1f;
     cursor: pointer;
+    transition: backdrop-filter 0.2s linear;
+    &:hover {
+        backdrop-filter: blur(10px);
+        span {
+            opacity: 1;
+        }
+    }
+    span {
+        transition: opacity 0.2s linear;
+        opacity: 0;
+    }
+`;
+
+const ContainerStyles = styled.div`
+    transition: transform 0.3s linear;
+    &:hover {
+        transform: scale(1.2);
+    }
 `;
 
 const PortfolioItem = ({ item }) => {
     const [readMore, setReadMore] = useState(false);
 
     return (
-        <Card background="light-1" style={{ position: 'relative' }} onMouseOver={() => setReadMore(true)}>
-            {readMore && <ReadMoreTextStyles onMouseOut={() => setReadMore(false)}>Read More</ReadMoreTextStyles>}
-            <CardHeader pad="small" style={{ cursor: 'pointer' }} title={item.title}>
-                {item.title}
-                <Image src={item.imageUrl} height="30" width={item.squareImage ? '30' : '100%'} />
-            </CardHeader>
-        </Card>
+        <ContainerStyles>
+            <Card background="light-1" style={{ position: 'relative' }} onMouseOver={() => setReadMore(true)}>
+                {readMore && (
+                    <ReadMoreTextStyles>
+                        <span>Read More</span>
+                    </ReadMoreTextStyles>
+                )}
+                <CardHeader pad="small" style={{ cursor: 'pointer' }} title={item.title}>
+                    {item.title}
+                    <Image src={item.imageUrl} height="30" width={item.squareImage ? '30' : '100%'} />
+                </CardHeader>
+            </Card>
+        </ContainerStyles>
     );
 };
 
