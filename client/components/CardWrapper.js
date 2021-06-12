57cc4f64a6d800982cde8687c46e976a0fb93448
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-06-12 20:43:00 +0200

    tweaks: no referrer on page link

--- client/components/CardWrapper.js
+++ client/components/CardWrapper.js
@@ -1,7 +1,14 @@
 import { isMobile, isTablet } from 'react-device-detect';
 import PropTypes from 'prop-types';
 
-const CardWrapper = ({ children, url }) => (isMobile || isTablet ? children : <a href={url}>{children}</a>);
+const CardWrapper = ({ children, url }) =>
+    isMobile || isTablet ? (
+        children
+    ) : (
+        <a href={url} style={{ textDecoration: 'none' }} target="_blank" rel="noreferrer noopener">
+            {children}
+        </a>
+    );
 
 CardWrapper.propTypes = {
     children: PropTypes.any,
