cd777c9d51915c3bebe594c8c2ac5c6113795458
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-08-31 09:08:46 +0200

    adds id to each meta entry for client side sorting

--- server/schemas/meta.js
+++ server/schemas/meta.js
@@ -2,24 +2,30 @@ import { CgNametag as icon } from 'react-icons/cg';
 
 export default {
     name: 'meta',
-    title: 'Home Page Metadata',
+    title: 'Metadata',
     type: 'document',
     icon,
     fields: [
         {
-            name: 'page',
+            name: 'title',
             title: 'Page',
             type: 'string',
             description: 'What page is this the metadata for?'
         },
         {
-            name: 'title',
+            name: 'id',
+            title: 'Data ID',
+            type: 'string',
+            description: 'ignore this, id to identify correct metadata for each page when building the application'
+        },
+        {
+            name: 'metaTitle',
             title: 'Title',
             type: 'string',
             description: 'What should the metadata title value be?'
         },
         {
-            name: 'description',
+            name: 'metaDescription',
             title: 'Description',
             type: 'string',
             description: 'What should the metadata description value be?'
